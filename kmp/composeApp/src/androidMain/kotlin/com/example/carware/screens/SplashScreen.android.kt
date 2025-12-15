3958debc9fa15b9141446b4faf70386937225d4a
mohamed ayman <mohamedaymancr7@wlu.ca>
2025-12-15 21:57:46 +0200

    added Splash Screen

---
+++ kmp/composeApp/src/androidMain/kotlin/com/example/carware/screens/SplashScreen.android.kt
@@ -0,0 +1,49 @@
+package com.example.carware.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.example.carware.R
+import kotlinx.coroutines.delay
+
+@Composable
+actual fun PlatformSplashAnimation(
+
+) {
+
+    Column(modifier = Modifier.fillMaxSize()
+        .background(Color(179, 0, 0, 255),),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        val composition by rememberLottieComposition(
+            LottieCompositionSpec.RawRes(R.raw.cw)
+        )
+
+        val progress by animateLottieCompositionAsState(
+            composition = composition,
+            iterations = 1,
+            isPlaying = true,
+            speed =0.9f,
+
+        )
+
+        LottieAnimation(
+            composition = composition,
+            progress = { progress },
+        )
+
+    }
+
+   }
--- kmp/composeApp/src/commonMain/kotlin/com/example/carware/screens/SplashScreen.kt
+++ kmp/composeApp/src/commonMain/kotlin/com/example/carware/screens/SplashScreen.kt
@@ -4,13 +4,15 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.navigation.NavController
-import com.example.carware.m
+import androidx.compose.ui.unit.dp
+import carware.composeapp.generated.resources.Res
+
 import com.example.carware.navigation.AddCarScreen
 import com.example.carware.navigation.HomeScreen
 import com.example.carware.navigation.OnboardingScreen
@@ -18,6 +20,8 @@ import com.example.carware.navigation.SignUpScreen
 import com.example.carware.util.LoginManager
 import kotlinx.coroutines.delay
 import kotlinx.serialization.Serializable
+import org.jetbrains.compose.resources.DrawableResource
+
 
 @Composable
 fun SplashScreen(
@@ -27,7 +31,7 @@ fun SplashScreen(
     LaunchedEffect(Unit) {
 
         // optional delay for UX
-        delay(1000)
+        delay(3000)
 
         val destination = when {
             !loginManager.isOnboardingComplete() -> OnboardingScreen
@@ -39,17 +43,10 @@ fun SplashScreen(
         onNavigate(destination)
     }
 
-
-    Column(m.fillMaxSize()
-        .background(Color.Yellow),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-            //            work here
-
-
-
-
-
-    }
+    PlatformSplashAnimation()
 }
+
+@Composable
+expect fun PlatformSplashAnimation(
+
+)
