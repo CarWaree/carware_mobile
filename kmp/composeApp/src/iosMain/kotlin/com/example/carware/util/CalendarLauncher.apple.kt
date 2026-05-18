3e3288e77c0c21e85d8a65ea206da0cd9ba84a96
Zyad osama <zyadosama@192.168.227.129>
2026-05-18 21:24:41 +0300

    changed ios calender handling

--- kmp/composeApp/src/iosMain/kotlin/com/example/carware/util/CalendarLauncher.apple.kt
+++ kmp/composeApp/src/iosMain/kotlin/com/example/carware/util/CalendarLauncher.apple.kt
@@ -1,10 +1,14 @@
 package com.example.carware.util
 
+import io.ktor.client.request.invoke
 import kotlinx.cinterop.ExperimentalForeignApi
 import platform.EventKit.EKEntityType
 import platform.EventKit.EKEvent
 import platform.EventKit.EKEventStore
+import platform.EventKit.EKRecurrenceEnd
 import platform.EventKit.EKSpan
+import platform.EventKit.EKRecurrenceFrequency
+import platform.EventKit.EKRecurrenceRule
 import platform.EventKitUI.EKEventEditViewAction
 import platform.EventKitUI.EKEventEditViewController
 import platform.EventKitUI.EKEventEditViewDelegateProtocol
@@ -15,7 +19,7 @@ import platform.UIKit.UIApplication
 import platform.darwin.NSObject
 import platform.darwin.dispatch_async
 import platform.darwin.dispatch_get_main_queue
-// Place delegate class here, outside CalendarLauncher
+
 private class EventEditDelegate : NSObject(), EKEventEditViewDelegateProtocol {
     override fun eventEditViewController(
         controller: EKEventEditViewController,
@@ -33,10 +37,11 @@ actual class CalendarLauncher actual constructor() {
     actual fun openCalendarWithEvent(
         title: String,
         description: String,
-        startTimeMillis: Long
+        startTimeMillis: Long,
+        repeatInterval: Int,
+        repeatUnit: String,
+        repeatCount: Int
     ) {
-
-
         val store = EKEventStore()
         store.requestAccessToEntityType(EKEntityType.EKEntityTypeEvent) { granted, _ ->
             if (!granted) return@requestAccessToEntityType
@@ -47,6 +52,25 @@ actual class CalendarLauncher actual constructor() {
             event.startDate = NSDate.dateWithTimeIntervalSince1970(startTimeMillis / 1000.0)
             event.endDate = NSDate.dateWithTimeIntervalSince1970((startTimeMillis / 1000.0) + 3600)
             event.calendar = store.defaultCalendarForNewEvents
+
+            if (repeatCount > 0) {
+                val frequency = when (repeatUnit.lowercase()) {
+                    "day" -> EKRecurrenceFrequency.EKRecurrenceFrequencyDaily
+                    "month" -> EKRecurrenceFrequency.EKRecurrenceFrequencyMonthly
+                    "year" -> EKRecurrenceFrequency.EKRecurrenceFrequencyYearly
+                    else -> EKRecurrenceFrequency.EKRecurrenceFrequencyDaily
+                }
+
+                val recurrenceEnd = EKRecurrenceEnd.recurrenceEndWithEndDate(
+                    NSDate.dateWithTimeIntervalSince1970(startTimeMillis / 1000.0 + (repeatInterval * repeatCount * 86400.0))
+                )
+                val recurrenceRule = EKRecurrenceRule(
+                    recurrenceWithFrequency = frequency,
+                    interval = repeatInterval.toLong(),
+                    end = recurrenceEnd
+                )
+                event.addRecurrenceRule(recurrenceRule)
+            }
 
             dispatch_async(dispatch_get_main_queue()) {
                 val editVC = EKEventEditViewController()
